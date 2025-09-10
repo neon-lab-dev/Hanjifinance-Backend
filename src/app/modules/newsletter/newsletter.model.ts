d1eb2da89d54a92cd977e5c8e4f02d07368a168c
rahulsd380 <rahulsd380@gmail.com>
2025-09-10 22:14:01 +0600

    Added newsletter apis

---
+++ src/app/modules/newsletter/newsletter.model.ts
@@ -0,0 +1,15 @@
+import { Schema, model } from "mongoose";
+import { TNewsletter } from "./newsletter.interface";
+
+const AreaSchema = new Schema<TNewsletter>(
+  {
+    name: { type: String, required: true, trim: true },
+    email: { type: String, required: true, trim: true },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Newsletter = model<TNewsletter>("Newsletter", AreaSchema);
+export default Newsletter;
---
+++ src/app/modules/newsletter/newsletter.route.ts
@@ -0,0 +1,14 @@
+import express from "express";
+import { NewsletterControllers } from "./newsletter.controller";
+import auth from "../../middlewares/auth";
+import { UserRole } from "../auth/auth.constants";
+
+const router = express.Router();
+
+router.post("/subscribe", auth(UserRole.admin, UserRole.moderator, UserRole.user), NewsletterControllers.subscribeNewsletter);
+router.get("/", auth(UserRole.admin, UserRole.moderator), NewsletterControllers.getAllNewsletters);
+router.get("/:id", auth(UserRole.admin, UserRole.moderator), NewsletterControllers.getSingleNewsletterById);
+router.put("/:id", auth(UserRole.admin, UserRole.moderator), NewsletterControllers.updateNewsletter);
+router.delete("/:id", auth(UserRole.admin, UserRole.moderator), NewsletterControllers.deleteNewsletter);
+
+export const NewsletterRoutes = router;
